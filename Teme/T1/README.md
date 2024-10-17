44c7867ff7d46e625540ad7cd3a7f7fb0bea82ae
Robert Takacs <RoTak@protonmail.com>
2024-10-17 21:30:20 +0300

    Update README and remove magic numbers

--- Teme/T1/README.md
+++ Teme/T1/README.md
@@ -10,9 +10,23 @@ Led-urile simple reprezintă gradul de încărcare al bateriei, pe care îl vom 
 
 Apăsarea scurtă a butonului de start va porni încărcarea. Apăsarea acestui buton în timpul încărcării nu va face nimic.
 
-Apăsarea lungă a butonului de stop va opri încărcarea forțat și va reseta stația la starea liberă. Apăsarea acestui buton cat timp stația este liberă nu va face nimic.
+Apăsarea lungă a butonului de stop va opri încărcarea forțat și va reseta stația la starea liberă. Apăsarea acestui buton cat timp stația este liberă nu va face nimic.\
 
-# 1.3 Componente Utilizate
+# 1.3. Structura Software
+
+Sunt definite 3 stări posibile pentru program, care se modifică fie prin input de la butoane, fie prin trecerea unor anumite intervale de timp.
+
+Stări posibile:
+
+- FREE: programul așteaptă apăsarea butonului de START pentru a trece în starea CHARGING
+- CHARGING: butonul START este ignorat. Se calculează la fiecare iterație timpul trecut dintre începutul CHARGING și timpul curent. Pe baza timpului trecut și intervale de timp definite (CHARGE_TIME_PER_LED, BLINK_PER_LED), se actualizează LED-ul care face blink și LED-urile care sunt pornite permanent. La finalizarea procesului pentru ultimul LED, se trece în starea ENDING. De asemenea, pe parcursul stării CHARGING, se face polling pentru starea butonului STOP. Dacă acesta a fost apăsat pentru o durată mai mare de 1 secundă (verificare prin debouncing), se trece direct în starea ENDING.
+- ENDING: Prin același procedeu ca în starea CHARGING, toate becurile fac blink, iar apoi se trece înapoi în starea FREE.
+
+### 1.3.1. Situații neașteptate
+
+LED-ul RGB folosit diferă față de cel din simulator. Astfel, acesta are pinii de Green și Blue inversați (R - Common - B - G). De aceea, a fost necesară actualizarea #define-urilor (modificat RGB_GREEN în 4 și RGB_BLUE în 5)
+
+# 1.4 Componente Utilizate
 
 - x4 LED-uri albastre
 - x1 LED RGB (Catod comun)
@@ -22,29 +36,29 @@ Apăsarea lungă a butonului de stop va opri încărcarea forțat și va reseta 
 - x1 Breadboard
 - Linii legătură
 
-# 1.4 Imagini Montaj
+# 1.5 Imagini Montaj
 
-## 1.4.1. Montaj
+## 1.5.1. Montaj
 
 <a href = "https://images.rotak.ro/github/robotica_1/Montaj.jpeg" target = "_blank"><img src = "https://images.rotak.ro/github/robotica_1/Montaj.jpeg" width = "300"></a>
 
-## 1.4.2. Detaliu Pini
+## 1.5.2. Detaliu Pini
 
 <a href = "https://images.rotak.ro/github/robotica_1/Detaliu_Pini.jpeg" target = "_blank"><img src = "https://images.rotak.ro/github/robotica_1/Detaliu_Pini.jpeg" width = "300"></a>
 
-## 1.4.3. Detaliu LED-uri
+## 1.5.3. Detaliu LED-uri
 
 <a href = "https://images.rotak.ro/github/robotica_1/Detaliu_LED-uri.jpeg" target = "_blank"><img src = "https://images.rotak.ro/github/robotica_1/Detaliu_LED-uri.jpeg" width = "300"></a>
 
-## 1.4.4. Detaliu Butoane
+## 1.5.4. Detaliu Butoane
 
 <a href = "https://images.rotak.ro/github/robotica_1/Detaliu_Butoane.jpeg" target = "_blank"><img src = "https://images.rotak.ro/github/robotica_1/Detaliu_Butoane.jpeg" width = "300"></a>
 
-# 1.5 Videoclip Montaj
+# 1.6 Videoclip Montaj
 
 Un videoclip YouTube Shorts care demonstrează funcționalitatea montajului poate fi vizionat la [următorul link](https://www.youtube.com/shorts/RiaUQndiVEw)
 
-# 1.6 Schema Electrica
+# 1.7 Schema Electrica
 
 <a href = "https://images.rotak.ro/github/robotica_1/Model.png" target = "_blank"><img src = "https://images.rotak.ro/github/robotica_1/Model.png" width = "300"></a>
 
--- Teme/T1/Tema 1 Arduino/src/main.cpp
+++ Teme/T1/Tema 1 Arduino/src/main.cpp
@@ -5,6 +5,8 @@
 #define LED3 8
 #define LED4 7
 
+// Din ceva motiv, LED-ul meu RGB are pinii
+// RED -> COMMON (-) -> BLUE -> GREEN
 #define RGB_RED 6
 #define RGB_GREEN 4
 #define RGB_BLUE 5
@@ -12,13 +14,23 @@
 #define BTN_START 3
 #define BTN_STOP 2
 
+// Timp debounce pentru butonul de start si butonul de stop
 #define DEBOUNCE_START_DELAY 50
 #define DEBOUNCE_STOP_DELAY 1000
 
+// Durata incarcarii unui LED
 #define CHARGE_TIME_PER_LED 3000
+// Durata blink-ului unui LED in starea CHARGING
 #define BLINK_PER_LED 300
+// Numarul de LED-uri de charging
+#define NO_CHARGING_LEDS 4
+// Durata blink-ului unui LED in starea ENDING
+#define ENDING_BLINK_PER_LED 500
+// Dupa cate blink-uri se termina starea ENDING
+#define ENDING_BLINKS 3
 
-enum StationState {
+enum StationState
+{
   FREE,
   CHARGING,
   ENDING
@@ -30,29 +42,29 @@ StationState state = FREE;
 char start_button_action = false;
 char stop_button_action = false;
 
-int start_button_state = HIGH;           
-int start_button_last_state = LOW;   
-unsigned long start_button_last_debounce_time;  
+int start_button_state = HIGH;
+int start_button_last_state = LOW;
+unsigned long start_button_last_debounce_time;
 
-
-int stop_button_state = HIGH;           
-int stop_button_last_state = LOW;   
-unsigned long stop_button_last_debounce_time;  
+int stop_button_state = HIGH;
+int stop_button_last_state = LOW;
+unsigned long stop_button_last_debounce_time;
 // end buttons
 
 // Charging state
 unsigned int charging_time_elapsed = 0;
 unsigned int charging_start_time = 0;
 unsigned short current_charging_led = 0;
+// Helper folosit pentru starea led-urilor (par si impar)
 unsigned short current_charging_blink_helper = 0;
 
-// Ending state 
+// Ending state
 unsigned int ending_time_elapsed = 0;
 unsigned int ending_start_time = 0;
 unsigned short ending_blink_helper = 0;
 
-
-void setup() {
+void setup()
+{
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
@@ -67,31 +79,36 @@ void setup() {
 
   start_button_last_debounce_time = millis();
   stop_button_last_debounce_time = millis();
-
 }
 
-void loop() {
-  // get the two button states
+void loop()
+{
+  /* DEBOUNCING PRELUAT DIN
+  https://deepbluembedded.com/arduino-button-debouncing/
+  */
 
   start_button_action = false;
   stop_button_action = false;
 
-
   // BUTON START
   int start_input = digitalRead(BTN_START);
   // If the switch changed, due to noise or pressing:
-  if (start_input != start_button_last_state) {
+  if (start_input != start_button_last_state)
+  {
     // reset the debouncing timer
     start_button_last_debounce_time = millis();
   }
- 
-  if ((millis() - start_button_last_debounce_time) > DEBOUNCE_START_DELAY) {
- 
+
+  if ((millis() - start_button_last_debounce_time) > DEBOUNCE_START_DELAY)
+  {
+
     // if the button state has changed:
-    if (start_input != start_button_state) {
+    if (start_input != start_button_state)
+    {
       start_button_state = start_input;
       // only toggle the LED if the new button state is HIGH
-      if (start_button_state == HIGH) {
+      if (start_button_state == HIGH)
+      {
         start_button_action = true;
       }
     }
@@ -103,18 +120,22 @@ void loop() {
   // BUTON STOP
   int stop_input = digitalRead(BTN_STOP);
   // If the switch changed, due to noise or pressing:
-  if (stop_input != stop_button_last_state) {
+  if (stop_input != stop_button_last_state)
+  {
     // reset the debouncing timer
     stop_button_last_debounce_time = millis();
   }
- 
-  if ((millis() - stop_button_last_debounce_time) > DEBOUNCE_STOP_DELAY) {
- 
+
+  if ((millis() - stop_button_last_debounce_time) > DEBOUNCE_STOP_DELAY)
+  {
+
     // if the button state has changed:
-    if (stop_input != stop_button_state) {
+    if (stop_input != stop_button_state)
+    {
       stop_button_state = stop_input;
       // only toggle the LED if the new button state is HIGH
-      if (stop_button_state == HIGH) {
+      if (stop_button_state == HIGH)
+      {
         stop_button_action = true;
       }
     }
@@ -123,19 +144,22 @@ void loop() {
 
   // END BUTON stop
 
-
-  if(state == FREE)
+  if (state == FREE)
   {
+    // LED-uri incarcare oprite
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
 
+    // led RGB verde
     digitalWrite(RGB_GREEN, HIGH);
     digitalWrite(RGB_RED, LOW);
     digitalWrite(RGB_BLUE, LOW);
 
-    if(start_button_action == true)
+    // daca e apasat butonul de start,
+    // trecem in CHARGING
+    if (start_button_action == true)
     {
       state = CHARGING;
       charging_start_time = millis();
@@ -154,22 +178,30 @@ void loop() {
   }
   else if (state == CHARGING)
   {
+    // verificam cat timp a trecut de la
+    // inceputul incarcarii
     charging_time_elapsed = millis() - charging_start_time;
 
     // la fiecare 3 secunde
-    if(charging_time_elapsed > CHARGE_TIME_PER_LED * (current_charging_led + 1))
+    if (charging_time_elapsed > CHARGE_TIME_PER_LED * (current_charging_led + 1))
     {
       digitalWrite(LED1 - current_charging_led, HIGH);
       current_charging_led += 1;
     }
 
-    if(charging_time_elapsed > BLINK_PER_LED * current_charging_blink_helper)
+    // pe baza helper-ului stim cand trebuie sa clipeasca
+    // led-ul activ
+    if (charging_time_elapsed > BLINK_PER_LED * current_charging_blink_helper)
     {
+      // LED1 - current_charging_led este led-ul curent
+      // Does not work in C probably
       digitalWrite(LED1 - current_charging_led, (current_charging_blink_helper % 2) ? HIGH : LOW);
       current_charging_blink_helper += 1;
     }
 
-    if(current_charging_led == 4 || (stop_button_action == true && charging_time_elapsed > DEBOUNCE_STOP_DELAY))
+    // daca toate cele 4 led-uri au fost incarcate,
+    // trecem in animatia de ending
+    if (current_charging_led == NO_CHARGING_LEDS || (stop_button_action == true && charging_time_elapsed > DEBOUNCE_STOP_DELAY))
     {
       state = ENDING;
       ending_time_elapsed = 0;
@@ -180,13 +212,12 @@ void loop() {
       digitalWrite(RGB_RED, HIGH);
       digitalWrite(RGB_BLUE, LOW);
     }
-
   }
-  else if(state == ENDING)
+  else if (state == ENDING)
   {
     ending_time_elapsed = millis() - ending_start_time;
 
-    if(ending_time_elapsed > 500 * ending_blink_helper)
+    if (ending_time_elapsed > ENDING_BLINK_PER_LED * ending_blink_helper)
     {
       short int to_write = ending_blink_helper % 2 ? HIGH : LOW;
       digitalWrite(LED1, to_write);
@@ -197,7 +228,8 @@ void loop() {
       ending_blink_helper += 1;
     }
 
-    if(ending_blink_helper == 7)
+    // un blink inseamna blink on + blink off (deci 2*500ms)
+    if (ending_blink_helper > ENDING_BLINKS * 2)
     {
       state = FREE;
 
@@ -206,6 +238,4 @@ void loop() {
       digitalWrite(RGB_BLUE, LOW);
     }
   }
-  
-
 }
